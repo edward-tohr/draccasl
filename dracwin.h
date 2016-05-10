166aba755fe928c85eb9d25945d9dfa4d8f44d4d
Jeff <edward.tohr@gmail.com>
2016-05-10 18:06:42 -0400

    Added basic header file

---
+++ dracwin.h
@@ -0,0 +1,16 @@
+#ifndef _DRACWIN_H_
+#define _DRACWIN_H_
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+#include iostream
+#include string
+#include sstream
+
+class DracWin {
+	
+	int winInit();
+
+};
+#endif
