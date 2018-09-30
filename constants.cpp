f23578ac951c600e7cd296b4cb0d8ddc0d7b8746
Jeff Morse <edward.tohr@gmail.com>
2018-09-30 17:05:48 -0400

    update dprint function to use stderr
    dPrint now accepts a boolean (defaulting to true) to see whether it'll print to stderr or stdout

--- constants.cpp
+++ constants.cpp
@@ -1,8 +1,19 @@
 #include "constants.h"
 #include "iostream"
 
+
+void dPrint(DEBUG_T dLvl, std::string msg, bool err = true) {
+	if (DEBUG >= dLvl) {
+    if (err) {
+		std::cerr << "errlog " << dLvl << ": " << msg << std::endl;
+    } else {
+      std::cout << "errlog " << dLvl << ": " << msg << std::endl;
+    }
+	}
+}
+
 void dPrint(DEBUG_T dLvl, std::string msg) {
 	if (DEBUG >= dLvl) {
-		std::cout << "errlog lv: " << dLvl << ": " << msg << std::endl;
+		std::cerr << "errlog " << dLvl << ": " << msg << std::endl;
 	}
 }
